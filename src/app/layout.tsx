d03522b0e113fcd550f4150d8d210bc646eacb87
Javier Jaramillo <jjaramillo7@schools.nyc.gov>
2025-10-20 22:02:08 -0400

    ✨ Add @vercel/analytics for enhanced tracking and analytics features
    - Include @vercel/analytics dependency in package.json and package-lock.json
    - Integrate Analytics component in the RootLayout for improved user interaction tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Providers from "@/components/providers";
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <Providers>
           <div className="min-h-screen flex flex-col">
             <Header />
